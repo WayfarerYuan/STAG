92233a93232ee659e0bfd637f374d4d4ede5ed97
WayfarerYuan <voyageryuan@outlook.com>
2023-04-17 17:35:14 +0100

    Nothing special, just getting started with this project.

--- src/main/java/edu/uob/GameServer.java
+++ src/main/java/edu/uob/GameServer.java
@@ -34,6 +34,7 @@ public final class GameServer {
     */
     public GameServer(File entitiesFile, File actionsFile) {
         // TODO implement your server logic here
+        //
     }
 
     /**
